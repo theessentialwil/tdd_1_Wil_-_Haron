c4b8c7a95bc6d1f081f15cccdfc815115dceba1c
mukulembezewil <wilmukulembeze@gmail.com>
2023-02-19 08:42:00 +0300

    Red: Test returned product 1 in lieu of 4

--- test/test.js
+++ test/test.js
@@ -7,8 +7,14 @@ const multiply = require('../multiply');
 // eslint-disable-next-line no-undef
 describe('My very first test-first assignment for number multiplication unit', () => {
   
+  // Test First TDD Cycle 1
   it('check that unit returns product of two number', () => {
     assert.equal(multiply(1, 1), 1);
   });
+
+  // Test First TDD Cycle 2
+  it('check that unit returns product of two numbers 2 and 2', () => {
+    assert.equal(multiply(2, 2), 4);
+  })
   
 });
