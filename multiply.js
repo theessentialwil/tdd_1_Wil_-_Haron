7ebb5096e8902211f1a35811dc56233b54d86bb6
mukulembezewil <wilmukulembeze@gmail.com>
2023-02-19 08:37:30 +0300

    Green. Test returns 1 as expected.

--- multiply.js
+++ multiply.js
@@ -1,1 +1,7 @@
 // test first assignment
+
+function multiply(a, b){
+  return 1;
+};
+
+module.exports = multiply;
--- test/test.js
+++ test/test.js
@@ -2,12 +2,13 @@
 /* eslint-disable no-undef */
 const { assert } = require('chai');
 // eslint-disable-next-line no-unused-vars
-const R = require('../multiply');
+const multiply = require('../multiply');
 
 // eslint-disable-next-line no-undef
 describe('My very first test-first assignment for number multiplication unit', () => {
   
   it('check that unit returns product of two number', () => {
-    assert.Equal(multiply(1, 1), 1);
+    assert.equal(multiply(1, 1), 1);
   });
+  
 });
